914cdfca363fb8a92afeb750b09dec1ec31038c7
Ivan <bdfy.valdis@gmail.com>
2022-10-24 11:17:23 +0700

    Add some tests for class Game

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,9 @@
 #define CTEST_MAIN
-
 #include "../third-party/ctest/ctest.h"
+#include <iostream>
 
 int main(int argc, const char** argv)
 {
+    setlocale(LC_ALL, "");
     return ctest_main(argc, argv);
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,37 @@
 #include "../third-party/ctest/ctest.h"
 
-CTEST(suite, simple)
+#include "../Life.h"
+#include <iostream>
+#include <sstream>
+
+CTEST(TestGame, testPrint)
 {
-    ASSERT_STR("foo", "foo");
+    Life life("tests/simple.txt");
+    std::wstringstream sstr;
+    life.Print(sstr);
+    std::wstring str = sstr.str();
+    std::wstring str1 = L"00000\n00100\n00100\n00100\n00000\n";
+    ASSERT_WSTR(str.c_str(), str1.c_str());
+}
+
+CTEST(TestGame, testMakeStep)
+{
+    Life life("tests/simple.txt");
+    std::wstringstream sstr;
+    life.MakeStep();
+    life.Print(sstr);
+    std::wstring str = sstr.str();
+    std::wstring str1 = L"00000\n00000\n01110\n00000\n00000\n";
+    ASSERT_WSTR(str.c_str(), str1.c_str());
+}
+
+CTEST(TestGame, testMakeStepBorderPoints)
+{
+    Life life("tests/border.txt");
+    std::wstringstream sstr;
+    life.MakeStep();
+    life.Print(sstr);
+    std::wstring str = sstr.str();
+    std::wstring str1 = L"00000\n00000\n00000\n00000\n00000\n";
+    ASSERT_WSTR(str.c_str(), str1.c_str());
 }
