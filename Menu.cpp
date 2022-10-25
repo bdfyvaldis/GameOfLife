6ef4eabbdd8069b021b4564d2e4aad180410cb52
Ivan <bdfy.valdis@gmail.com>
2022-10-25 11:28:23 +0700

    Add class Menu

---
+++ Menu.cpp
@@ -0,0 +1,5 @@
+#include "Menu.h"
+
+void Menu::Print(std::wostream& stream)
+{
+}
---
+++ Menu.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+
+class Menu {
+public:
+    void Print(std::wostream& stream = std::wcout);
+};
