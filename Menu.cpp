9a3f90ab9cbdcb97ae2af4a1f32cb55cc5193941
Ivan <bdfy.valdis@gmail.com>
2022-10-26 09:50:09 +0700

    Delete class Menu

--- Menu.cpp
+++
@@ -1,7 +0,0 @@
-#include "Menu.h"
-
-void Menu::Print(std::wostream& stream)
-{
-    stream << L"\033[;30m\033[0;47m\033[1m <space> \033[0m - new generation"
-           << std::endl;
-}
--- Menu.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-
-class Menu {
-public:
-    void Print(std::wostream& stream = std::wcout);
-};
